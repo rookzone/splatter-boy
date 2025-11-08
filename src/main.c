d711c254f72fefc2e2d1251770b41c58bffa65f6
Rookzone <danial.dawson@outlook.com>
2025-11-08 18:02:21 +0000

    dd

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 ﻿#include <gb/gb.h>
+#include <stdio.h>
 #include "tiles/SillyTiles.h"  // your header file
 
 
-typedef enum {
+enum {
     PLAYER_SPRITE = 0,
     ITEM_SPRITE = 1
 } Sprites;
 
-typedef enum {
+enum {
     TILE_SMILE = 0,
     TILE_HEART = 1
 } Tiles;
@@ -25,6 +26,8 @@ void main(void) {
 
     set_sprite_tile(ITEM_SPRITE, TILE_HEART);   // Take tile 1 from sprite memory and display it as sprite number 1
     move_sprite(ITEM_SPRITE, 95 + 8, 75); 
+
+    printf("Testing a GameBoy \ngame.");
 
     DISPLAY_ON;  // Turn the LCD back on
 	SHOW_SPRITES;
