199b26ab7f756b1c977b89594c51662acd88dc34
rookzone <77877551+rookzone@users.noreply.github.com>
2025-12-08 08:15:59 +0000

    init commit

--- src/game_data.h
+++ src/game_data.h
@@ -5,6 +5,7 @@
 
 #include <gb/gb.h>
 #include "custom_types.h"
+#include "game_object.h"
 
 // Game Object list sizes
 #define NUM_BALLS 18 // HIGHER THAN 18 LAGS - DO NOT SET TO ODD NUMBER
@@ -26,6 +27,21 @@ typedef struct {
     uint16_t next_sprite_id;
 
 } Graphics;
+
+typedef struct {
+    // Object pool (all game objects live here)
+    GameObject pool[MAX_GAME_OBJECTS];
+    uint8_t total_count;             // Total active objects
+    
+    /** === REGISTRIES ===
+     * These registries are used to hold the index (location) of a certain type of GameObject
+     * On creation an object should register it's index in the main object pool into this array.
+     * This allows for parts of the game to iterate through only this object type without expensive object type checks
+     */
+    uint8_t ball_indices[MAX_BALLS];
+    uint8_t ball_count;
+
+} ObjectManager;
 
 typedef struct {
 
