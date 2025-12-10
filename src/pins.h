5adedbf6202712d3718f8470305bc2e506d30bb7
Rookzone <danial.dawson@outlook.com>
2025-12-10 00:32:13 +0000

    refactor nearly complete

--- src/pins.h
+++ src/pins.h
@@ -10,10 +10,4 @@
 
 #include "custom_types.h"
 
-// Pass an array of type Pin to initialise the pins.
-void init_pin(Pin* pin, GameSprite* gfx_data, uint8_t pin_x, uint8_t pin_y);
-
-// Pass a pin to initialise without graphics (for pins created using background)
-void init_background_pin(Pin* pin, uint8_t pin_x, uint8_t pin_y);
-
 #endif // PINS_H_
