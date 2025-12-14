1be7bf8e38b66344495d17cd396aa6a883546824
Rookzone <danial.dawson@outlook.com>
2025-12-14 20:09:13 +0000

    fix

--- src/input.h
+++ src/input.h
@@ -1,9 +1,6 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-void get_key_down(void);
-
-
 static inline uint8_t get_key_down(uint8_t key)
 {
     return(game.system.keys & key && !(game.system.previous_keys & key));
